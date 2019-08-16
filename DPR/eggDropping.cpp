a9d563787caa6f4f36c32df7606f2df3dc78d07b
shivanshu <shivanshu1333@gmail.com>
2019-08-16 06:16:43 +0530

    egg dropping recursive n>2, else use formula

--- DPR/eggDropping.cpp
+++ DPR/eggDropping.cpp
@@ -2,26 +2,29 @@
 #include<vector>
 #include<climits>
 #include<algorithm>
-
 using namespace std;
 
 int eggDrop(int n, int k){
-    if(k==0 || k==1)
-        return k;
     if(n==1)
+        return k;
+    if(k==1 || k==0)
         return k;
     int min = INT_MAX, res;
 
-    for(int x=1;x<=k;x++){
-        res = max(eggDrop(n-1,x-1),eggDrop(n,k-x));
+    for(int i=1;i<=k;i++){
+        res = max(eggDrop(n-1,i-1),eggDrop(n,k-i));
         if(res<min)
             min = res;
     }
-return min+1;
+    return min+1;
 }
 
 int main(){
-    int egg = 2;
-    int flooe = 10;
-    cout<<eggDrop(2,10)<<endl;
+    int T;
+    cin>>T;
+    while(T--){
+        int n,k;
+        cin>>n>>k;
+        cout<<eggDrop(n,k)<<endl;
+    }
 }
