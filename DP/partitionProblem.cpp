983cf1f10f169c20ed43d1f598bffb53a467e0ce
shivanshu <shivanshu1333@gmail.com>
2019-08-16 08:03:42 +0530

    for dp[i][j] check v[i-1]

--- DP/partitionProblem.cpp
+++ DP/partitionProblem.cpp
@@ -11,10 +11,10 @@ bool partition(vector<int> &v, int tar){
         dp[i][0]=1;
     for(int i=1;i<dp.size();i++){
         for(int j=1;j<=tar;j++){
-            if((j-v[i])<0)
+            if((j-v[i-1])<0)
                 dp[i][j] = dp[i-1][j];
             else{
-                dp[i][j] = dp[i-1][j] || dp[i-1][j-v[i]];
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-v[i-1]];
             }
         }
     }
